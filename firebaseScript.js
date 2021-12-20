0a1de5eca232f67c13472759bfe9564bf807fcff
myduckperson <illarionov0405@gmail.com>
2021-12-20 17:46:59 +0200

    read template firebase

--- firebaseScript.js
+++ firebaseScript.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"
+import { doc, getDoc, getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +23,17 @@ const app = initializeApp(firebaseConfig);
 
 
 const auth = getAuth(app);
+
+const db = getFirestore(app);
+const docRef = doc(db, "main", "tasks");
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  console.log("Document data:", docSnap.data());
+} else {
+  // doc.data() will be undefined in this case
+  console.log("No such document!");
+}
 
 // google pop up
 // google pop up
